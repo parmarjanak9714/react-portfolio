05d83c9f95d671d5501c9f996c320d1fd07d0e92
janak  Parmar <parmarjanak9714@gmail.com>
2026-04-21 15:50:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Home from './Pages/Home'
 import About from './Pages/About'
 import Skills from './Pages/Skills'
 import Projects from './Pages/Projects'
-import Contact from './pages/Contact'
+import Contact from './Pages/Contact'
 
 
 function App() {
